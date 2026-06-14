7613b8675b75c4e68b6b7b100944b7bf30f8a4f5
ananyasurana <136243851+ananyasurana@users.noreply.github.com>
2026-06-14 22:51:10 +0530

    Add custom exceptions for account errors

--- exceptions.py
+++ exceptions.py
@@ -1,1 +1,9 @@
+class InsufficientFundsError(Exception):
+    """Raised when a SavingsAccount withdrawal would violate the Rs.500 minimum balance."""
+    pass
+
+
+class OverdraftLimitError(Exception):
+    """Raised when a CurrentAccount withdrawal would exceed the overdraft limit."""
+    pass
 
